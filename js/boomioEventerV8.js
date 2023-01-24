ee4898859d70fdb35ccd49db5273330b22d5665f
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-01-24 19:34:27 +0200

    Fix build issue

--- js/boomioEventerV8.js
+++ js/boomioEventerV8.js
@@ -2,7 +2,7 @@ const localStoragePropertyName = 'boomioPluginConfig';
 
 /////////// Scripts ////////
 // const puzzleScript = 'https://cdn.jsdelivr.net/gh/boomio-api-v2/final-combined-wdigets-1@main/js/puzzlePluginV2.js';
-const puzzleScript = './js/puzzlePluginV2.js';
+const puzzleScript = 'https://cdn.jsdelivr.net/gh/boomio-api-v2/final-combined-wdigets-1@main/js/puzzlePluginV3.js';
 
 const wheelScript = 'https://cdn.jsdelivr.net/gh/boomio-api-v2/final-combined-wdigets@main/js/wheelOfFortunePluginV6.js';
 ///////////////////////////
