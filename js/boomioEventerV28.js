feab92080fdb2d2d1ac4d5c872839647a3d26a7e
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-01-25 13:20:06 +0200

    Fixed build issue

--- js/boomioEventerV28.js
+++ js/boomioEventerV28.js
@@ -219,19 +219,13 @@ class Boomio extends LocalStorageConfig {
             "extra_data": data
         };
 
-        let test_data = {
-            "user_session": "6851ef2f-f6c7-49e8-b78b-9d08a4005275",
-            "current_page_url": "https://wheel-of-fortune1234.myshopify.com/products/puzzle4",
-            "extra_data": data
-        }
-
         return new Promise(async (resolve) => {
             const rawResponse = await  fetch(apiLink, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(test_data)
+                body: JSON.stringify(request_data)
             });
             resolve(rawResponse.json())
         })
