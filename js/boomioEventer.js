46bf4889f4e3d8b83e24dbb9a66c43561583677b
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-01-31 15:51:15 +0200

    Fix changing boomio

--- js/boomioEventer.js
+++ js/boomioEventer.js
@@ -6,7 +6,7 @@ const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera
 /////////// Scripts ////////
 const imageWidgetScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/8e352f90cfadb73c4afb4e4133e3a3af742937f2/js/imagePlugin.js?min=1';
 
-const puzzleScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/DK/development/new-puzzle-widget-ui/js/puzzlePlugin.js';
+const puzzleScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/6951fc5e9c4bdd9d97671cd8fe3b42b06dbf9d34/js/puzzlePlugin.js';
 
 const wheelScript = 'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/main/js/wheelOfFortunePlugin.js?min=1';
 
