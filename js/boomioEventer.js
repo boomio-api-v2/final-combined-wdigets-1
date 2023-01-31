12380461a9778fe92361c1131b43d2c99531046c
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-01-31 13:16:57 +0200

    Update plugin

--- js/boomioEventer.js
+++ js/boomioEventer.js
@@ -6,7 +6,7 @@ const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera
 /////////// Scripts ////////
 const imageWidgetScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/8e352f90cfadb73c4afb4e4133e3a3af742937f2/js/imagePlugin.js?min=1';
 
-const puzzleScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/148d13657e249c11e822c3a33e2fca9c14a2b9fa/js/puzzlePlugin.js';
+const puzzleScript = './js/puzzlePlugin.js';
 
 const wheelScript = 'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/main/js/wheelOfFortunePlugin.js?min=1';
 
@@ -268,21 +268,21 @@ class Boomio extends LocalStorageConfig {
 
      async setInitialConfiguration() {
         try {
-            ////Test data ??????
-            // super.updateConfig({
-            //     puzzles_collected: 2,
-            //     appearing_puzzle_nr: 3,
-            //     success: true,
-            //     animation: 1
-            // })
-            // createScript(puzzleScript);
+            //Test data ??????
+            super.updateConfig({
+                puzzles_collected: 0,
+                appearing_puzzle_nr: 1,
+                success: true,
+                animation: 1
+            })
+            createScript(puzzleScript);
 
-            const content = await this.send({ go_hunt: "true"});
-            super.setConfigFromApi(content);
-            if (content?.widget_type && content.instruction !== 'stop') {
-                const scriptUrl = this.getScriptUrl(content.widget_type)
-                createScript(scriptUrl)
-            }
+            // const content = await this.send({ go_hunt: "true"});
+            // super.setConfigFromApi(content);
+            // if (content?.widget_type && content.instruction !== 'stop') {
+            //     const scriptUrl = this.getScriptUrl(content.widget_type)
+            //     createScript(scriptUrl)
+            // }
         } catch (err) {
             console.log(err)
         }
