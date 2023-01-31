5648dcb0cb8cc4b5f624377e9de0e4f6b10357c8
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-01-31 15:04:18 +0200

    Minimize frame img size

--- js/boomioEventer.js
+++ js/boomioEventer.js
@@ -6,7 +6,7 @@ const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera
 /////////// Scripts ////////
 const imageWidgetScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/8e352f90cfadb73c4afb4e4133e3a3af742937f2/js/imagePlugin.js?min=1';
 
-const puzzleScript = './js/puzzlePlugin.js';
+const puzzleScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/d4cdc7d7144b9699556ee497912b18a42cf1456d/js/puzzlePlugin.js';
 
 const wheelScript = 'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/main/js/wheelOfFortunePlugin.js?min=1';
 
