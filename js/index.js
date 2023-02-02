46157a7723135785e0f7686301c7bc3d9d420a5d
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-02 10:28:18 +0200

    Update main script file

--- js/index.js
+++ js/index.js
@@ -3,20 +3,21 @@ const localStoragePropertyName = 'boomioPluginConfig';
 const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 
-/////////// Scripts ////////
-const qrCodeScript = './js/qrcode.min.js';
+/////////// Modules Scripts ////////
+const qrCodeScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/31e4e15bef157c67ef66db1e5767f9e003e3b24b/js/qrcode.min.js';
 
-const localStorageScript = './js/modules/localStorage.js';
+const localStorageScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/31e4e15bef157c67ef66db1e5767f9e003e3b24b/js/modules/localStorage.js';
 
-const stylesEnviroimentScript = './js/modules/stylesEnviroiment.js';
+const stylesEnviroimentScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/31e4e15bef157c67ef66db1e5767f9e003e3b24b/js/modules/stylesEnviroiment.js';
 
-const animationScript = './js/modules/animation.js';
+const animationScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/31e4e15bef157c67ef66db1e5767f9e003e3b24b/js/modules/animation.js';
 
-const draggableScript = './js/modules/draggable.js';
+const draggableScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/31e4e15bef157c67ef66db1e5767f9e003e3b24b/js/modules/draggable.js';
 
-const boomioEventerScript = './js/modules/boomioEventer.js';
+const boomioEventerScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/31e4e15bef157c67ef66db1e5767f9e003e3b24b/js/modules/boomioEventer.js';
+/////////////////////////////////
 
-
+///////////// Widgets Script ///////////////
 const imageWidgetScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/8e352f90cfadb73c4afb4e4133e3a3af742937f2/js/imagePlugin.js?min=1';
 
 const puzzleScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/e7e5b809dc42982fbea3d0c3892d7d0253ecb94d/js/puzzlePlugin.js';
@@ -26,13 +27,14 @@ const wheelScript = 'https://raw.githubusercontent.com/boomio-api-v2/final-combi
 const startWidgetScript = 'https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/8e352f90cfadb73c4afb4e4133e3a3af742937f2/js/startWidget.js?min=1';
 ///////////////////////////
 
+/////////Images  /////////
 const appStoreImage =
     'https://github.com/boomio-api-v2/easter-egg-styles/blob/main/img/appstore.png?raw=true';
 const playStoreImage =
     'https://github.com/boomio-api-v2/easter-egg-styles/blob/main/img/playstore.png?raw=true';
 const dotImage =
     'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/DK/development/new-puzzle-widget-ui/images/boomio-app.png?raw=true';
-
+//////////////////////////
 const createScript = (url) => {
     const script = document.createElement('script');
     script.src = url;
--- js/puzzlePlugin.js
+++ js/puzzlePlugin.js
@@ -1,7 +1,7 @@
 const frameSvg =
     "https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/DK/development/new-puzzle-widget-ui/images/puzzle/frame.png?raw=true";
 
-const puzzleCssLink = "https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/c9fe1bb1c6755baeba12c5ae7710d0f466ef802c/css/puzzle.css";
+const puzzleCssLink = "https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/31e4e15bef157c67ef66db1e5767f9e003e3b24b/css/puzzle.css";
 
 //////// Services ////////
 const { addStylesToHtml, addCssLinkToHtml, assignStyleOnElement } = stylesEnvironment;
