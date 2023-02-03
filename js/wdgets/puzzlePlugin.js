d627b5deaec7d3e7a819cfcdc156d6f3c04561c1
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-03 10:10:08 +0200

    Changes images link

--- js/wdgets/puzzlePlugin.js
+++ js/wdgets/puzzlePlugin.js
@@ -8,9 +8,10 @@ import {
 import { boomio } from '../index';
 import {
     isMobileDevice,
-    dotImage } from '../config';
-const frameSvg =
-    "https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/DK/development/new-puzzle-widget-ui/images/puzzle/frame.png?raw=true";
+    dotImage
+} from '../config';
+
+const frameSvg = "/images/puzzle/frame.png";
 
 const puzzleCssLink = "https://rawcdn.githack.com/boomio-api-v2/final-combined-wdigets-1/31e4e15bef157c67ef66db1e5767f9e003e3b24b/css/puzzle.css";
 
