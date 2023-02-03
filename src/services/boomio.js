3d1e99dd5ee8d88a3d7eba33f36d98b79f505ff2
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-03 16:14:14 +0200

    Update build issue

--- src/services/boomio.js
+++ src/services/boomio.js
@@ -60,18 +60,11 @@ class BoomioService {
   setInitialConfiguration() {
     try {
       window.onload = async () => {
-        localStorageService.setConfigFromApi({
-          success: true,
-          puzzles_collected: 3,
-          appearing_puzzle_nr: 4,
-          animation: 1,
-        });
-        this.loadWidget();
-        // const content = await this.send({ go_hunt: "true"});
-        // localStorageService.setConfigFromApi(content);
-        // if (content?.widget_type && content.instruction !== 'stop') {
-        //     this.loadWidget(content.widget_type)
-        // }
+        const content = await this.send({ go_hunt: "true"});
+        localStorageService.setConfigFromApi(content);
+        if (content?.widget_type && content.instruction !== 'stop') {
+            this.loadWidget(content.widget_type)
+        }
       };
     } catch (err) {
       console.log(err);
