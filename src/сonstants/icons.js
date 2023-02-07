25647f19d0562e56782df483676e27f6daeef61a
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-07 18:38:10 +0200

    Updated bundle

--- src/сonstants/icons.js
+++ src/сonstants/icons.js
@@ -13,7 +13,7 @@ export const frameSvg =
   'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/a2af819a6a3f005406f98a86cb82061c855fd065/images/puzzle/frame.svg';
 
 export const dotImage =
-  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/DK/development/new-puzzle-widget-ui/images/boomio-app.png?raw=true';
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/0753f565c8eeb594222696c37839677a8738f41d/images/dotImage.svg';
 
 export const puzzleIconsList = [
   'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/a2af819a6a3f005406f98a86cb82061c855fd065/images/puzzle/puzzle-1.svg',
