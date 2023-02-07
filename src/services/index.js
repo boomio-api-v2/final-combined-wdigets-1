593e146045e8752daf892c7c47f9f79d421b977e
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-07 12:28:14 +0200

    Added qrcode service

--- src/services/index.js
+++ src/services/index.js
@@ -4,3 +4,4 @@ export * from './localStorage';
 export { DragElement } from './draggable';
 export { default as boomioService } from './boomio';
 export { default as AnimationService } from './animation';
+export { default as QrCodeModal } from './qrCodeModal';
