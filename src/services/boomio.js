ab6e18a945430690877d361440d47899e72a8c08
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-10 17:38:44 +0200

    increase bundle size

--- src/services/boomio.js
+++ src/services/boomio.js
@@ -65,16 +65,11 @@ class BoomioService {
   setInitialConfiguration() {
     try {
       window.onload = async () => {
-        // const content = await this.send({ go_hunt: 'true' });
-        // localStorageService.setConfigFromApi(content);
-        // if (content?.widget_type && content.instruction !== 'stop') {
-        //   this.loadWidget(content.widget_type);
-        // }
-        localStorageService.setConfigFromApi({
-          success: true,
-          puzzles_collected: 3,
-          appearing_puzzle_nr: 4,
-        });
+        const content = await this.send({ go_hunt: 'true' });
+        localStorageService.setConfigFromApi(content);
+        if (content?.widget_type && content.instruction !== 'stop') {
+          this.loadWidget(content.widget_type);
+        }
         this.loadWidget();
       };
     } catch (err) {
