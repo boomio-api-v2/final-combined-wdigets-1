e4bd576863b6c283adee6f3bbcc6e6a70a372a10
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-13 17:30:35 +0200

    Updated crash animation

--- src/widgets/iceWidget/constants.js
+++ src/widgets/iceWidget/constants.js
@@ -7,8 +7,7 @@ export const imagesList = [
   'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/step2.png?raw=true',
 ];
 
-export const cracksImage =
-  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/cracks.png?raw=true';
-
 export const hammerImage =
   'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/hammer.png?raw=true';
+
+export const iceBlockImage = '/images/iceWidget/ice-block.png';
