16bf4abc80272b2a3bfa35e61870353580a26ada
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-13 15:33:27 +0200

    Updated changes

---
+++ src/widgets/iceWidget/constants.js
@@ -0,0 +1,13 @@
+export const imagesList = [
+  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/step7.png?raw=true',
+  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/step6.png?raw=true',
+  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/step5.png?raw=true',
+  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/step4.png?raw=true',
+  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/step3.png?raw=true',
+  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/step2.png?raw=true',
+];
+
+export const cracksImage = '/images/iceWidget/cracks.png';
+
+export const hammerImage =
+  'https://github.com/boomio-api-v2/final-combined-wdigets-1/blob/ice_widget/images/iceWidget/hammer.png?raw=true';
