50d40757f486f2015a4a18a5f8878c55a7e345af
denys-wetelo <122462631+denys-wetelo@users.noreply.github.com>
2023-02-15 14:40:26 +0200

    Update bundle

--- src/services/boomio.js
+++ src/services/boomio.js
@@ -67,18 +67,11 @@ class BoomioService {
   setInitialConfiguration() {
     try {
       window.onload = async () => {
-        // const content = await this.send({ go_hunt: 'true' });
-        // localStorageService.setConfigFromApi(content);
-        // if (content?.widget_type && content.instruction !== 'stop') {
-        //   this.loadWidget(content.widget_type);
-        // }
-        localStorageService.setConfigFromApi({
-          success: true,
-          puzzles_collected: 3,
-          appearing_puzzle_nr: 4,
-          p_coupon_text: 'Discount',
-          img: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg',
-        });
+        const content = await this.send({ go_hunt: 'true' });
+        localStorageService.setConfigFromApi(content);
+        if (content?.widget_type && content.instruction !== 'stop') {
+          this.loadWidget(content.widget_type);
+        }
         this.loadWidget('ice');
       };
     } catch (err) {
