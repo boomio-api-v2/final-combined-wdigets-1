77f674d02200103ab40a1bcc5ddc9ad2a71f701e
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2023-08-30 14:30:31 +0300

    moving machine

--- src/widgets/clawMachineWidget/clawMachineWidget.js
+++ src/widgets/clawMachineWidget/clawMachineWidget.js
@@ -14,7 +14,6 @@ class ClawMachineWidget {
     this.clawDivPosition = { top: 0, left: 0 };
     this.moveDivWithArrows();
     this.setupClickHandler();
-    this.setupSwipeHandler();
     this.animationInProgress = false; // Use a separate flag for animation
 
     // Store an array of grabbable present divs
@@ -22,6 +21,129 @@ class ClawMachineWidget {
 
     // Initialize flags for each present div
     this.isHoldingclawPresentDivs = Array(this.clawPresentDivs.length).fill(false);
+    this.startAutomaticClawMovement();
+    this.setupControlButton();
+  }
+
+  setupControlButton() {
+    // Create a button element
+    const controlButton = document.createElement('button');
+    controlButton.innerText = 'Get a prize';
+    controlButton.classList.add('control-button');
+
+    // Apply CSS for the button's appearance
+    controlButton.style.position = 'fixed';
+    controlButton.style.bottom = '50%';
+    controlButton.style.left = '50%';
+    controlButton.style.transform = 'translate(-50%, -50%)';
+    controlButton.style.width = '100px';
+    controlButton.style.height = '100px';
+    controlButton.style.borderRadius = '50%';
+    controlButton.style.backgroundColor = 'red';
+    controlButton.style.color = 'white';
+    controlButton.style.fontSize = '16px';
+    controlButton.style.cursor = 'pointer';
+
+    // Add a click event listener to trigger the claw's grabbing action
+    controlButton.addEventListener('click', () => {
+      this.activateGrabbing();
+    });
+
+    // Append the button to the document body
+    document.body.appendChild(controlButton);
+  }
+
+  activateGrabbing() {
+    if (this.animationInProgress || this.isHoldingclawPresentDivs.some((held) => held)) {
+      return;
+    }
+
+    this.animationInProgress = true;
+
+    // Add your logic to move the claw down here
+    this.clawDiv.style.transition = 'top 1s';
+
+    const isMobile = window.innerWidth <= 768; // Adjust the threshold as needed
+
+    this.clawDiv.style.top = `calc(100vh - ${isMobile ? '110px' : '110px'})`;
+    this.chainDiv.style.transition = 'height 1s, transform 1s';
+    this.chainDiv.style.height = `calc(100vh - ${isMobile ? '110px' : '110px'})`;
+
+    setTimeout(() => {
+      this.clawPresentDivs.forEach((clawPresentDiv, index) => {
+        const clawDivRect = this.clawDiv.getBoundingClientRect();
+        const clawPresentDivRect = clawPresentDiv.getBoundingClientRect();
+
+        if (
+          clawDivRect.left < clawPresentDivRect.right &&
+          clawDivRect.right > clawPresentDivRect.left &&
+          clawDivRect.top < clawPresentDivRect.bottom
+        ) {
+          if (!this.isHoldingclawPresentDivs[index]) {
+            if (this.clawDiv.contains(clawPresentDiv)) {
+              this.clawDiv.removeChild(clawPresentDiv);
+            }
+            this.isHoldingclawPresentDivs[index] = true;
+
+            clawPresentDiv.style.transition = 'top 0s';
+            clawPresentDiv.style.top = '10px';
+            clawPresentDiv.style.left = '10px';
+            this.clawDiv.appendChild(clawPresentDiv);
+
+            this.gameTimer = setTimeout(() => {
+              setTimeout(() => {
+                this.clawDiv.removeChild(clawPresentDiv);
+                this.isHoldingclawPresentDivs[index] = false;
+              }, 1000);
+
+              this.endGame(index);
+            }, 1000);
+          }
+        }
+      });
+
+      setTimeout(() => {
+        this.clawDiv.style.transition = 'top 1s';
+        this.clawDiv.style.top = '50px';
+
+        this.chainDiv.style.transition = 'height 1s, transform 1s';
+        this.chainDiv.style.height = '50px';
+        this.chainDiv.style.transform = 'translateY(0)';
+        this.animationInProgress = false;
+      }, 600);
+    }, 1000);
+  }
+  startAutomaticClawMovement() {
+    let direction = 1; // 1 for right, -1 for left
+    const clawSpeed = 2; // Adjust the speed as needed
+    let clawPosition = 0;
+    const maxX = window.innerWidth - this.clawDiv.clientWidth;
+
+    const moveClaw = () => {
+      clawPosition += direction * clawSpeed;
+
+      // Ensure the claw stays within the screen boundaries
+      if (clawPosition >= maxX) {
+        clawPosition = maxX;
+        direction = -1; // Reverse direction when reaching the right edge
+      } else if (clawPosition <= 0) {
+        clawPosition = 0;
+        direction = 1; // Reverse direction when reaching the left edge
+      }
+
+      // Update the claw's position
+      this.clawDiv.style.left = `${clawPosition}px`;
+
+      // Update the chain's position to move together with the claw
+      const chainDivLeft = clawPosition + this.clawDiv.clientWidth / 2;
+      this.chainDiv.style.left = `${chainDivLeft}px`;
+
+      // Schedule the next movement
+      setTimeout(moveClaw, 50); // Adjust the delay as needed for the desired speed
+    };
+
+    // Start the automatic movement
+    moveClaw();
   }
   createContainer = () => {
     const clawMachineContainer = document.createElement('div');
@@ -47,7 +169,7 @@ class ClawMachineWidget {
 
     // Calculate the width of the line div
     const lineWidth = window.innerWidth;
-    const presentSpacing = 30; // Adjust as needed
+    const presentSpacing = 20; // Adjust as needed
     const presentWidth = 50; // Adjust as needed
     function isMobile() {
       const mobileThreshold = 768; // You can adjust this threshold as needed
@@ -59,16 +181,9 @@ class ClawMachineWidget {
     let presentColors;
 
     if (isMobile()) {
-      numberOfPresents = 3;
-      presentColors = ['black', 'black', 'purple'];
+      numberOfPresents = 7;
     } else {
-      numberOfPresents = 4;
-      presentColors = ['black', 'black', 'black', 'purple'];
-    }
-
-    for (let i = presentColors.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [presentColors[i], presentColors[j]] = [presentColors[j], presentColors[i]];
+      numberOfPresents = 9;
     }
 
     // Array to store present positions
@@ -104,7 +219,7 @@ class ClawMachineWidget {
 
         clawPresentDiv.style.left = `${leftPosition}px`;
         clawPresentDiv.style.bottom = `${Math.random() * 15 + 25}px`;
-        clawPresentDiv.style.backgroundColor = presentColors[i];
+        clawPresentDiv.style.backgroundColor = 'orange';
 
         clawLineDiv.appendChild(clawPresentDiv);
       }
@@ -169,97 +284,7 @@ class ClawMachineWidget {
     });
   }
 
-  setupClickHandler() {
-    console.log('click');
-    const startAnimation = () => {
-      if (this.animationInProgress || this.isHoldingclawPresentDivs.some((held) => held)) {
-        console.log('return');
-        return;
-      }
-      this.animationInProgress = true;
-      setTimeout(() => {
-        this.animationInProgress = false;
-        this.clawDiv.style.pointerEvents = 'auto';
-      }, 2000);
-      this.clawDiv.style.pointerEvents = 'none';
-
-      // Move the Claw div down
-      this.clawDiv.style.transition = 'top 1s';
-      function isMobile() {
-        const mobileThreshold = 768; // You can adjust this threshold as needed
-
-        return window.innerWidth <= mobileThreshold;
-      }
-
-      this.clawDiv.style.top = `calc(100vh - ${isMobile() ? '110px' : '110px'})`;
-
-      // Extend the chain-div height and transform it down as the Claw div goes down
-      this.chainDiv.style.transition = 'height 1s, transform 1s'; // Add transitions for height and transform change
-      this.chainDiv.style.height = `calc(100vh - ${isMobile() ? '110px' : '110px'})`;
-
-      // Delay the collision detection until after the animation
-      console.log('2', this.animationInProgress);
-
-      setTimeout(() => {
-        this.clawPresentDivs.forEach((clawPresentDiv, index) => {
-          const clawDivRect = this.clawDiv.getBoundingClientRect();
-          const clawPresentDivRect = clawPresentDiv.getBoundingClientRect();
-
-          if (
-            clawDivRect.left < clawPresentDivRect.right &&
-            clawDivRect.right > clawPresentDivRect.left &&
-            clawDivRect.top < clawPresentDivRect.bottom
-          ) {
-            console.log(`Collision detected with Present Div ${index}`);
-
-            // Check if the present div is already being held
-            if (!this.isHoldingclawPresentDivs[index]) {
-              // The Claw div is touching the claw Present Div
-              if (this.clawDiv.contains(clawPresentDiv)) {
-                this.clawDiv.removeChild(clawPresentDiv);
-              }
-              this.isHoldingclawPresentDivs[index] = true;
-
-              // Adjust the styles for the claw Present Div
-              clawPresentDiv.style.transition = 'top 0s';
-              clawPresentDiv.style.top = '10px';
-              clawPresentDiv.style.left = '10px';
-              this.clawDiv.appendChild(clawPresentDiv);
-
-              // Add a timeout for the game timer for this present div
-              this.gameTimer = setTimeout(() => {
-                const presentColor = clawPresentDiv.style.backgroundColor;
-                if (presentColor === 'black') {
-                  setTimeout(() => {
-                    this.clawDiv.removeChild(clawPresentDiv);
-                    this.isHoldingclawPresentDivs[index] = false;
-                  }, 1000);
-                } else {
-                  this.endGame(index);
-                }
-              }, 1000);
-            }
-          }
-        });
-
-        setTimeout(() => {
-          this.clawDiv.style.transition = 'top 1s';
-          this.clawDiv.style.top = '50px';
-
-          // Shrink the chain-div height and transform it up as the Claw div goes back up
-          this.chainDiv.style.transition = 'height 1s, transform 1s'; // Add transitions for height and transform change
-          this.chainDiv.style.height = '50px';
-          this.chainDiv.style.transform = 'translateY(0)'; // Reset the chain-div's vertical position
-
-          // Reset the global flag to allow the next animation
-        }, 600); // Delay for 1 second
-      }, 1000); // Delay for 1 second
-    };
-
-    // Add the click event listener initially
-
-    this.clawDiv.addEventListener('click', startAnimation);
-  }
+  setupClickHandler() {}
 
   endGame = () => {
     setTimeout(() => {
@@ -270,58 +295,6 @@ class ClawMachineWidget {
       }
     }, 700);
   };
-
-  setupSwipeHandler() {
-    if (this.animationInProgress) {
-      console.log('return');
-      return;
-    }
-    let isDragging = false;
-    let startX = 0;
-    let currentX = 0;
-
-    const startDrag = (clientX) => {
-      isDragging = true;
-      startX = clientX;
-      currentX = clientX;
-    };
-
-    const moveDrag = (clientX) => {
-      if (!isDragging) return;
-
-      const deltaX = clientX - currentX;
-      currentX = clientX;
-
-      const maxX = window.innerWidth - this.clawDiv.clientWidth;
-
-      // Calculate the new position while keeping it within screen boundaries
-      this.clawDivPosition.left += deltaX;
-      this.clawDivPosition.left = Math.min(Math.max(this.clawDivPosition.left, 0), maxX);
-
-      // Update the position of the .claw-div
-      this.clawDiv.style.transition = 'left 0s'; // Disable vertical transition during dragging
-      this.clawDiv.style.left = `${this.clawDivPosition.left}px`;
-
-      // Update the position of the .chain-div to move together with the claw-div
-      const chainDivLeft = this.clawDivPosition.left + this.clawDiv.clientWidth / 2; // Adjust as needed
-      this.chainDiv.style.left = `${chainDivLeft}px`;
-    };
-
-    const endDrag = () => {
-      if (isDragging) {
-        isDragging = false;
-        this.clawDiv.style.transition = 'left 1s'; // Add transition when releasing
-      }
-    };
-
-    this.clawDiv.addEventListener('mousedown', (e) => startDrag(e.clientX));
-    document.addEventListener('mousemove', (e) => moveDrag(e.clientX));
-    document.addEventListener('mouseup', () => endDrag());
-
-    this.clawDiv.addEventListener('touchstart', (e) => startDrag(e.touches[0].clientX));
-    document.addEventListener('touchmove', (e) => moveDrag(e.touches[0].clientX));
-    document.addEventListener('touchend', () => endDrag());
-  }
 }
 
 export default () => {
