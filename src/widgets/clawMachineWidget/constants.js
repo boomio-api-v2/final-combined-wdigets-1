cf5ccd2459270fa0686795709cfc78fc73baff11
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2023-09-07 10:36:41 +0300

    changes

--- src/widgets/clawMachineWidget/constants.js
+++ src/widgets/clawMachineWidget/constants.js
@@ -5,7 +5,7 @@ export const chainImg = [
   'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/claw-machine-auto-with-close/images/clawWidget/claw_detail.svg',
 ];
 export const buttonImg = [
-  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/claw-machine-auto-with-close/images/clawWidget/Button.svg',
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/claw-machine-auto-with-close/images/clawWidget/ButtonBackground.svg',
 ];
 export const clawPoleImg = [
   'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/claw-machine-auto-with-close/images/clawWidget/clawPole.png',
