d6458cff399f8d53b0f7777526815495e3045330
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2023-09-20 15:57:22 +0300

    changes

--- src/services/boomio.js
+++ src/services/boomio.js
@@ -65,13 +65,18 @@ class BoomioService extends UserService {
           }
           localStorageService.setConfigFromApi(content);
           if (content?.widget_type && content.instruction !== 'stop') {
+            console.log('1');
             this.loadWidget(content.widget_type);
           } else if (localStorage.getItem('testing_Widgets')) {
+            console.log('2');
+
             this.loadWidget('testing');
           } else if (
             localStorage.getItem('closing_button') &&
             localStorage.getItem('start_widget')
           ) {
+            console.log('3');
+
             this.loadWidget('start_widget');
           }
           this.config = localStorageService.getDefaultConfig();
@@ -87,6 +92,8 @@ class BoomioService extends UserService {
   }
 
   testing(testingWidget) {
+    console.log('4');
+
     this.loadWidget(testingWidget);
   }
 
