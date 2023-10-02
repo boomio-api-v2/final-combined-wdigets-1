949951a55030e04c4a20a98e711b7611a2546a2b
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2023-10-02 18:30:44 +0300

    changes

--- src/widgets/testingWidget/testingWidget.js
+++ src/widgets/testingWidget/testingWidget.js
@@ -10,7 +10,7 @@ class TestingWidget {
     localStorage.setItem('testing_Widgets', true);
     const width = 260;
     const height = 300;
-    console.log('testing');
+    console.log('testing1');
     const { clientWidth, clientHeight } = document.documentElement;
 
     const posx = ((clientWidth - width) / 2).toFixed();
