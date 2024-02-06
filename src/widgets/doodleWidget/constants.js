b85ac00cfe8a440f902e5fabf4d4577f2e3b4e0b
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2024-02-06 11:59:14 +0200

    changes

--- src/widgets/doodleWidget/constants.js
+++ src/widgets/doodleWidget/constants.js
@@ -14,7 +14,7 @@ export const background =
   'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/JUMP UP_Barbora_x2.png';
 
 export const backgroundRed =
-  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/Background_Barbora_New.png';
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/Background_Barbora.png';
 
 export const couponBackground =
   'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/COUPON.png';
--- src/widgets/helpers/constants.js
+++ src/widgets/helpers/constants.js
@@ -1,11 +1,11 @@
 export const rules =
-  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/Rules.png';
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/Rules_green.png';
 
 export const rules2 =
-  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/Desktop_rules.png';
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/Rules_green_desktop.png';
 
 export const rules2Mobile =
-  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/Rules_mobile.png';
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/Mobile_rules_greeen.png';
 
 export const gameOver =
-  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/GAME_OVER_RED.png';
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/qr-remove/images/doodleWidget/Barbora/GAME_OVER_GREEN.png';
