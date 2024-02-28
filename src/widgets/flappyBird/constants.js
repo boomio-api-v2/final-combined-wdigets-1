dd8355e05846e4089c15d3ea46cbf0cb595ed10f
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2024-02-28 15:44:21 +0200

    changes

--- src/widgets/flappyBird/constants.js
+++ src/widgets/flappyBird/constants.js
@@ -1,5 +1,5 @@
 export const close = [
-  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/claw-machine-icon-button/images/startWidget/close.png',
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/claw-machine-icon-button/images/flappyWidget/closeImage.png',
 ];
 
 export const introGif =
