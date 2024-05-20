7a79ba73be076345551431649dbbe6d41c2eaec2
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2024-05-20 13:11:09 +0300

    changes

--- src/widgets/helpers/CompetitionScoreTableContainer.js
+++ src/widgets/helpers/CompetitionScoreTableContainer.js
@@ -60,9 +60,9 @@ export class CompetitionScoreTableContainer {
     if (userBestPlace > 20) {
       tableHTML += `
             <tr style="background: rgba(255, 255, 255, 1);box-shadow:none;margin: 0;height:44px ">
-            <td style="padding-left:17px;text-align:start;width: 25px; color: rgba(61, 73, 40, 1); border: none;font-size: 18px; font-family: Georama; font-weight: 800; text-transform: uppercase; line-height: 27px; word-wrap: break-word">${userBestPlace}</td>
+            <td style="padding-left:17px;text-align:start;width: 25px; color: rgba(61, 73, 40, 1); border: none;font-size: 16px; font-family: Georama; font-weight: 800; text-transform: uppercase; line-height: 27px; word-wrap: break-word">${userBestPlace}</td>
 
-              <td style="padding-left:10px;text-align:start;width: 142px; color: ${
+              <td style="padding-left:10px;text-align:start;width: 130px; color: ${
                 this.prop === 'Barbora' ||
                 this.prop === 'F1' ||
                 this.prop === 'Fantazijos' ||
