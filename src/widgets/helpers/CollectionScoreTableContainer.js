8f68fa2b5432f97a1601838a8b64d186934dae4e
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2024-05-31 02:40:49 +0300

    changes

--- src/widgets/helpers/CollectionScoreTableContainer.js
+++ src/widgets/helpers/CollectionScoreTableContainer.js
@@ -202,7 +202,7 @@ export class CollectionScoreTableContainer {
               </div>
       <div  style="margin-left:14px;height: ${'302px'}; top: 114px; position: absolute; border-right:none;">
         <div class="boomio-custom-scrollbar">
-          <table style="margin:10px">
+          <table style="margin:10px;border-spacing:4px;border-collapse:separate">
             <tbody class="boomio-tbody">
     `;
 
