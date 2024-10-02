75bbcc92eced0fcb3f30a7fb9074b6cae688992b
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2024-10-02 15:37:32 +0300

    changes

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -113,7 +113,7 @@ class LocalStorageService {
     const widget_subtype = config?.subtype ?? false;
     const email_collection_required = config?.email_collection_required ?? false;
     const product = config?.product ?? '???';
-    const language = config?.language ?? 'LV';
+    const language = config?.language ?? '';
 
     return {
       language,
