64c318c021a27eedfc68542dd6d70128b8dc44db
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2024-10-21 16:19:38 +0300

    changes

--- src/widgets/helpers/CompetitionCodeScoreTableContainer.js
+++ src/widgets/helpers/CompetitionCodeScoreTableContainer.js
@@ -339,7 +339,7 @@ export class CompetitionCodeScoreTableContainer {
       }
 
 ${
-  this.scoreTable.user_best_score
+  this.scoreTable.user_best_score > 1500
     ? `<div style="box-sizing: border-box;width: 100%; padding-left: 12px; padding-right: 12px; padding-top: 7px; padding-bottom: 7px; background:#A40033; border-radius: 32px; border: 0.50px  rgba(255, 255, 255, .6) solid; justify-content: space-between; align-items: center; display: inline-flex;width:260px;position:absolute;top:505px;left:calc(50% - 130px);">
 <div style="height: 17px; color: white; font-size: 16px; font-family: Montserrat; font-weight: 600; line-height: 16px; word-wrap: break-word" id="p_code_text">
  ${this.userDiscountCode ?? 'CODE'}
