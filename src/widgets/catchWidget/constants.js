84bfeac576d97febf85dae78f0ee797baead856c
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2024-10-21 16:16:15 +0300

    changes

--- src/widgets/catchWidget/constants.js
+++ src/widgets/catchWidget/constants.js
@@ -22,7 +22,7 @@ export const backgroundPienoZvaigzdes =
 export const playerPienoZvaigzdes =
   'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/whack-testing/images/catchWidget/PienoZvaigzdes/player.png';
 export const introPegasas =
-  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/whack-testing/images/catchWidget/Pegasas/catchIntro.png';
+  'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/whack-testing/images/catchWidget/Pegasas/catchIntro.gif';
 
 export const backgroundPegasas =
   'https://raw.githubusercontent.com/boomio-api-v2/final-combined-wdigets-1/feature/whack-testing/images/catchWidget/Pegasas/background.png';
