7f946c174792f09c97cd443b2a86bfe8528251c2
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2024-11-29 14:46:57 +0200

    changes

--- src/widgets/helpers/DidYouKnowContainer.js
+++ src/widgets/helpers/DidYouKnowContainer.js
@@ -1552,8 +1552,10 @@ ${
     }; position: absolute; border-right:none;">
         <div class="boomio-custom-scrollbar">
           <table style="margin-top:${
-            this.prop === 'Pigu.lt' ? (this.isMobileWidthSmall ? '60px' : '40px') : '30px'
-          };border-spacing:3px;width:calc(100% - 80px);margin-left:40px;border-collapse:separate">
+            this.prop === 'Pigu.lt' ? (this.isMobileWidthSmall ? '30px' : '40px') : '30px'
+          };border-spacing:3px;width:${
+      this.isMobileWidthSmall ? 'calc(100% - 60px)' : 'calc(100% - 80px)'
+    };margin-left:${this.isMobileWidthSmall ? '20px' : '40px'};border-collapse:separate">
             <tbody class="boomio-tbody">
             <div class='closeDidYouKnow' style='pointer-events: none;position:absolute;z-index:9999999;right:${
               this.isMobileWidthSmall ? '20px' : '40px'
