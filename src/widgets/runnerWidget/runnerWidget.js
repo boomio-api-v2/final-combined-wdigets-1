42713c4f555c04d6a228201b8085bd8269da606c
Aurimas Baraniunas <aurimas.baraniunas@gmail.com>
2025-04-25 12:45:09 +0300

    changes

--- src/widgets/runnerWidget/runnerWidget.js
+++ src/widgets/runnerWidget/runnerWidget.js
@@ -885,6 +885,19 @@ ${
     );
     window.addEventListener('resize', Resize);
 
+    function setVhUnit() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    window.addEventListener('resize', setVhUnit);
+    window.addEventListener('orientationchange', () => {
+      // Add a small delay so iOS finishes rotating
+      setTimeout(setVhUnit, 300);
+    });
+
+    setVhUnit(); // initial call
+
     const checkOrientationAndPause = () => {
       setTimeout(() => {
         // Force reflow by updating canvas or main container dimensions
