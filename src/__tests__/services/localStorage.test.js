25bb27f52dba40ae1a9dcc9cf7e505a3499d8007
Andrius Sunauskas <andrius.sunauskas@destiny.eu>
2025-09-22 09:12:42 +0300

    Fix tests

--- src/__tests__/services/localStorage.test.js
+++ src/__tests__/services/localStorage.test.js
@@ -10,19 +10,16 @@ describe('localStorageService', () => {
     expect(typeof result.app_url).toMatch('string');
     expect(typeof result.custom_text).toMatch('string');
     expect(typeof result.puzzle.puzzles_collected).toMatch('number');
-    expect(typeof result.puzzle.puzzles_needed).toMatch('number');
+    expect(typeof result.puzzle.puzzles_needed).toMatch('undefined');
     expect(typeof result.puzzle.hint).toMatch('string');
 
-    expect(
-      typeof result.appearing_puzzle_nr === 'number' || result.appearing_puzzle_nr === null,
-    ).toBe(true);
     expect(typeof result.x_position === 'number' || result.x_position === null).toBe(true);
     expect(typeof result.y_position === 'number' || result.y_position === null).toBe(true);
     expect(typeof result.img === 'string' || result.img === null).toBe(true);
     expect(typeof result.w_button_text).toMatch('string');
     expect(typeof result.w_hint_static_text).toMatch('string');
     expect(typeof result.w_top_text).toMatch('string');
-    expect(typeof result.p_coupon_text).toMatch('string');
+    expect(typeof result.p_coupon_text).toMatch('object');
     expect(typeof result.p_code_text).toMatch('string');
     expect(typeof result.p_bottom_text === 'string' || result.p_bottom_text === null).toBe(true);
     expect(typeof result.p_button_text === 'string' || result.p_button_text === null).toBe(true);
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,6 @@
 export * from './qrCodeModal';
 
 export { DragElement } from './draggable';
-export { default as boomioService } from './boomio';
 export { default as localStorageService } from './localStorage';
 export { default as widgetHtmlService } from './widgetHtml';
 
