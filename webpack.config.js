a6ab1c29a102f1dc8d340564edb62f95a91db74c
AndriusBoomio <andrius@boomio.com>
2025-10-06 10:03:15 +0300

    Obfuscator

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,38 @@
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const WebpackObfuscator = require('webpack-obfuscator');
 
 module.exports = {
   mode: 'development',
   entry: './src/app.js',
-  plugins: [new ESLintPlugin()],
+  plugins: [
+    new ESLintPlugin(),
+    new WebpackObfuscator(
+      {
+        // Low obfuscation preset - Best Performance
+        compact: true,
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        debugProtectionInterval: 0,
+        disableConsoleOutput: false,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: false,
+        renameGlobals: false,
+        selfDefending: false,
+        simplify: true,
+        splitStrings: false,
+        stringArray: false,
+        stringArrayThreshold: 0,
+        unicodeEscapeSequence: false,
+      },
+      [
+        // Exclude external libraries (node_modules)
+        'node_modules/**',
+      ],
+    ),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
