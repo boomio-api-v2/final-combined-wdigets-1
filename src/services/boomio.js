0926ea509b3738a3c1f13bb5dea667a286e1c2e9
AndriusBoomio <andrius@boomio.com>
2025-10-28 11:48:53 +0200

    Add f: 'boomio_security_v1'

--- src/services/boomio.js
+++ src/services/boomio.js
@@ -366,6 +366,7 @@ class BoomioService extends UserService {
             c: signature,
             d: generateFakeIPv4(timestamp),
             e: generateFakeIPv6(timestamp),
+            f: 'boomio_security_v1',
           }
         : extra_data,
     };
