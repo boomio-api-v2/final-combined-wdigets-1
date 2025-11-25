ef4270da3e3177e9502d4d54aa2fc7697ed28e61
AndriusBoomio <andrius@boomio.com>
2025-11-25 09:35:07 +0200

    Fix crash

--- src/widgets/runnerWidget/runnerWidget.js
+++ src/widgets/runnerWidget/runnerWidget.js
@@ -1975,7 +1975,7 @@ ${
         // Adjust object placement condition
         if (objects.length === 0 || objects.at(-1).x < canvas.width - 200) {
           objects.push(new GameObject(barriersSprites[0], (4 * canvas.width) / 2.5, canvas.height - wrapperBlock.offsetHeight / 2.7, false));
-          var randomBarrier = RandomInteger(1, barriersSprites.length + 1);
+          var randomBarrier = RandomInteger(1, barriersSprites.length);
           switch (randomBarrier) {
             case 1:
               objects.at(-1).image = barriersSprites[randomBarrier - 1];
